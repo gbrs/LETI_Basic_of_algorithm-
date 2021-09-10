14705c073a5c99ce8a4a8d1039046d2c2d787917
gbrs <gbrs@mail.ru>
2021-09-10 23:41:01 +0300

    studying the code from lesson 4.5

---
+++ Probe/Probe/Probe.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int value = -1; // Значение числа
+	unsigned int order = 32; // Количество разрядов
+	unsigned int mask = 1 << order - 1; // Маска побитового сравнения
+	for (int i = 1; i <= order; i++)
+	{
+		putchar(value & mask ? '1' : '0');
+		value <<= 1; // Побитовый сдвиг числа
+		if (i % 8 == 0)
+		{
+			putchar(' ');
+		}
+		if (i % order - 1 == 0)
+		{
+			putchar(' ');
+		}
+	}
+	return 0;
+}
