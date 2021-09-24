05697f0ff0c0b9f9c71025a67116a69c9162506e
gbrs <gbrs@mail.ru>
2021-09-24 19:48:14 +0300

    make output

--- Probe/Laba_3/Laba_3.cpp
+++ Probe/Laba_3/Laba_3.cpp
@@ -10,18 +10,19 @@ int main()
     {
         cout << "Enter 6, 8 or 10: ";
 
-        unsigned int n;
-        cin >> n;
+        unsigned int N;
+        cin >> N;
         // n = 10;
 
         cout << endl;
 
-        vector<vector<int>> v(n, vector<int>(n));
-        // int v[10][10];
+        // vector<vector<int>> v(n, vector<int>(n));
+        int v[10][10] = {0};
 
-        size_t i = 0, j = n - 1;
+        size_t i = 0, j = N - 1;
 
         int value = 1;
+        unsigned int n = N;
 
         while (n != 0)
         {
@@ -31,12 +32,16 @@ int main()
             for (k = 0; k < n - 1; k++) v[i][j++] = value++;
             for (k = 0; k < n - 1; k++) v[i--][j] = value++;
 
-            ++i; --j; n = n < 2 ? 0 : n - 2;
+            ++i; --j;
+            n = n < 2 ? 0 : n - 2;
         }
 
-        for (const auto& row : v)
+        for (int a = 0; a < N; a++)
         {
-            for (int x : row) cout << setw(3) << x << ' ';
+            for (int b = 0; b < N; b++)
+            {
+                cout << setw(4) << v[a][b];
+            }
             cout << endl;
         }
 
